cb3a0b556cf6cc8231f6ef819fc6fbad36cd6bdf
ezselltranscript-SG <ezselltranscript@staffingglobal.org>
2025-05-06 09:51:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ app.add_middleware(
     max_age=3600,
 )
 
-app.mount("/", StaticFiles(directory="src", html=True), name="frontend")
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
+
 
 
 # Incluir rutas
