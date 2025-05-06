2be722b6712c53f019ba883ca486a2832411e3c1
ezselltranscript-SG <ezselltranscript@staffingglobal.org>
2025-05-06 12:13:44 -0500

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000';
+
+const API_BASE_URL = 'https://die-botschaft-transcript-ai-app.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
